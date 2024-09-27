1c079335c7cb71343b678c7270766b176168f23f
privatgt <92684876+privatgt@users.noreply.github.com>
2024-09-27 11:56:18 +0400

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,83 @@
+let playlist;
+function play(list){
+    // Load the YouTube IFrame API
+    playlist=list;
+const tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+let currentVideoIndex = 0;
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        playerVars: {
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    const videoIds = playlist.map(item => item.videoId);
+    player.loadPlaylist(videoIds, 0, playlist[0].start);
+    player.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.PLAYING) {
+        startTimeCheck();
+        handleVideoChange();
+    } else if (event.data === YT.PlayerState.ENDED) {
+        handleNextVideo();
+    } else {
+        stopTimeCheck();
+    }
+}
+
+function handleVideoChange() {
+    const currentVideoId = player.getVideoData().video_id;
+    currentVideoIndex = playlist.findIndex(item => item.videoId === currentVideoId);
+    if (currentVideoIndex !== -1) {
+        const currentVideo = playlist[currentVideoIndex];
+        player.seekTo(currentVideo.start);
+    }
+}
+
+function handleNextVideo() {
+    currentVideoIndex++;
+    if (currentVideoIndex < playlist.length) {
+        player.playVideoAt(currentVideoIndex);
+        setTimeout(() => player.seekTo(playlist[currentVideoIndex].start), 10);
+    } else {
+        stopTimeCheck();
+        player.stopVideo();
+    }
+}
+
+let timeCheckInterval;
+function startTimeCheck() {
+    stopTimeCheck();
+    timeCheckInterval = setInterval(checkTime, 100);
+}
+
+function stopTimeCheck() {
+    if (timeCheckInterval) {
+        clearInterval(timeCheckInterval);
+    }
+}
+
+function checkTime() {
+    const currentTime = player.getCurrentTime();
+    const currentVideo = playlist[currentVideoIndex];
+
+    if (currentTime >= currentVideo.end && currentVideo.end!=-1) {
+        handleNextVideo();
+    } else if (currentTime < currentVideo.start) {
+        player.seekTo(currentVideo.start);
+    }
+}
+
+
